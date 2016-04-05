95d5b4c4afce21845b1b7535ef0d53143d3f933e
andreas <andreas@romab.com>
2016-04-05 19:13:14 +0200

    Make ifmap-server logfile configurable.

--- manifests/config/config.pp
+++ manifests/config/config.pp
@@ -45,6 +45,7 @@ class contrail::config::config (
   $device_manager_config   = {},
   $svc_monitor_config      = {},
   $basicauthusers_property = [],
+  $ifmap_server_logfile    = '/var/log/contrail/ifmap-server.log',
 ) {
 
   validate_hash($api_config)
